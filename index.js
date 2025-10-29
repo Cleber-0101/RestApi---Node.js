c79ed746df04c1949b3d27434aa4e5f786acdf60
Cleber-0101 <cleberbatista99@hotmail.com>
2025-10-29 15:36:23 -0300

    Praticando conceitos de Api Rest ultilizando insomnia - Body request

--- index.js
+++ index.js
@@ -2,17 +2,29 @@ const express = require('express')
 
 const server = express()
 
+
+
 //Req query params 
-server.get('/curso', (req,res) => {
+server.get('/teste', (req,res) => {
     const nome = req.query.nome
     return res.json({Curso: `Curso de ${nome}`})
 })
 
 //Req params
-// localhost:3000/curso/2
-server.get('/curso/:id' , (req,res) => {
+//localhost:3000/curso/2
+server.get('/teste/:id' , (req,res) => {
     const id = req.params.id
-    return res.json({curso: `Curso ${id}`})
+    return res.json({cai_aqui : `teste ${id}`})
+})
+
+
+const nomesFamilia = ["cleber" ,"jessica" , "Nour Batista"];
+
+//Req Body
+server.get('/nomes/:index', (req,res) => {
+    const { index } = req.params;
+    //pega item da lista a partir do seu indexador
+    return res.json(nomesFamilia[index]);
 })
 
 server.listen(3000)
